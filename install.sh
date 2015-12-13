0243a40d069170f5a1b3dc4a29311b1988b9a683
Eric Silva <eric.silva@pfizer.com>
2015-12-13 13:58:01 -0500

    fixed ansible vars

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 CURRENT=`pwd`
 
+echo "\033[0;32m Updating apt-get.......\033[0m"
+sudo apt-get update
+
 if [ ! -d ~/.config-desktop-home ]; then
-    echo "\033[0;32m Installing python.......\033[0m"
+    echo "\033[0;32m Installing python, git, and open-ssh.......\033[0m"
     sudo apt-get install -y -qq python python-pip git git-core openssh-server
 
     echo "\033[0;32m Installing ansible.......\033[0m"
     sudo pip install ansible
 
-    echo "\033[0;32m Clone the installation config.\033[0m"
+    echo "\033[0;32m Clone the ansible installation config.......\033[0m"
     sudo -u `whoami` -H git clone https://github.com/ericjsilva/ansible-desktop-ubuntu.git $HOME/.config-desktop-home
 else
     echo "\033[0;32mThe folder ".config-desktop-home" is already installed\033[0m"
