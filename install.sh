a7aeb4aca70eb7f421a0f5f31ad9ccdf4aa68ce4
Eric Silva <eric.silva@pfizer.com>
2019-01-05 12:09:58 -0500

    re-added link for sublime text

--- install.sh
+++ install.sh
@@ -40,7 +40,6 @@ echo "\033[0;32m Installing additional software.......\033[0m"
 cd ~
 #wget https://download.sublimetext.com/sublime-text_build-3176_i386.deb
 #sudo -H dpkg -i sublime-text_build-3126_i386.deb
-#sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime
 
 # New Sublime Install
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -48,6 +47,8 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime
+
 
 # Install Meteor
 # curl https://install.meteor.com/ | sh
