e79272ec8b24069a76f870595b55f0661334d5c8
Eric Silva <eric.silva@pfizer.com>
2019-01-05 12:07:35 -0500

    updated sublime text install

--- install.sh
+++ install.sh
@@ -38,9 +38,16 @@ ansible-playbook -i hosts site.yml -c local -K
 echo "\033[0;32m Installing additional software.......\033[0m"
 # Install Sublime Text
 cd ~
-wget https://download.sublimetext.com/sublime-text_build-3176_i386.deb
-sudo -H dpkg -i sublime-text_build-3126_i386.deb
-sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime
+#wget https://download.sublimetext.com/sublime-text_build-3176_i386.deb
+#sudo -H dpkg -i sublime-text_build-3126_i386.deb
+#sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime
+
+# New Sublime Install
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 
 # Install Meteor
 # curl https://install.meteor.com/ | sh
